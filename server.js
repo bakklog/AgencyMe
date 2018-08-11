c548df6431da106556bde32a3abcb37dce622714
Stephan Moerman <stephan@moerman.cc>
2018-08-11 16:23:12 +0200

    added devdependency for nodemon

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 // packages
+
+const dotenv = require('dontenv');
+
+
+// Load enviroment varibals from .env file,  where API key and passwords (should be stored)
+dotenv.load({
+    path: '.env'
+});
